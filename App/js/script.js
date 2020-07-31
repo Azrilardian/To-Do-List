2924170e261994d3c0164aab3dcd8fe6cbb1ac6f
Azrilardian <irfan05ariell@gmail.com>
2020-07-31 18:31:25 +0800

    Update JS : Menambah fitur ubah warna pada saat user membuat list, menambahkan fitur hapus list, dan menghapus semua hal yang tidak perlu

--- App/js/script.js
+++ App/js/script.js
@@ -1,45 +1,94 @@
 function toDoListApp() {
 
-    const listContainer = document.querySelector('.create');
-    const input = document.querySelector('.margin .input textarea');
+    // Variabel
+    const create = document.querySelector('.create');
+    const input = document.querySelector('.input input');
     const btnShowInput = document.querySelector('.left button');
-    let editable = false;
+    const closeIcon = document.querySelector('.create header p');
+    const btnCreateList = document.querySelector('#create');
+    const listColor = document.querySelectorAll('.color span');
 
-    function showInput() {
-        const closeIcon = document.querySelector('.create header p');
-        btnShowInput.addEventListener('click', () => {
-            defaultText();
-            showInputAlert(listContainer, input);
-        });
-        listContainer.addEventListener('keyup', (e) => {
-            if (e.keyCode == 13) {
-                createList(input.value);
-                closeList(listContainer, input);
+    // Listener
+    btnShowInput.addEventListener('click', () => showInputAlert(create, input));
+    input.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+            createList(input);
+            closeList(create, input);
+        };
+    });
+    btnCreateList.addEventListener('click', () => {
+        createList(input);
+        closeList(create, input);
+    });
+    closeIcon.addEventListener('click', () => closeList(create, input));
+    listColor.forEach(color => {
+        color.addEventListener('click', function() {
+            const colorId = color.id;
+            switch (colorId) {
+                case "yellow":
+                    input.style.backgroundColor = '#ffffa9';
+                    input.focus();
+                    break;
+                case "green":
+                    input.style.backgroundColor = '#b4ffb4';
+                    input.focus();
+                    break;
+                case "blue":
+                    input.style.backgroundColor = '#a3ceff';
+                    input.focus();
+                    break;
+                case "black":
+                    input.style.backgroundColor = '#6d6d6d';
+                    input.focus();
+                    break;
+                case "grey":
+                    input.style.backgroundColor = '#d3d3d3';
+                    input.focus();
+                    break;
+                case "white":
+                    input.style.backgroundColor = '#ffffff';
+                    input.focus();
+                    break;
             };
+            for (let i = 0; i < listColor.length; i++) listColor[i].classList.remove('active');
+            this.classList.add('active');
         });
-        closeIcon.addEventListener('click', () => closeList(listContainer, input));
-    }
-    showInput();
+    });
 
+    document.addEventListener('click', e => {
+        const target = e.target;
+        if (target.classList.contains('list')) target.classList.toggle('completed');
+        if (target.classList.contains('fa-trash')) removeList(target);
+    })
+
+    // Function
     const showInputAlert = (e, i) => {
         e.classList.toggle('active');
-        setTimeout(function() {
-            i.focus();
-        }, 10)
+        setTimeout(() => i.focus(), 15);
     };
 
-    function closeList(listContainer, input) {
-        listContainer.classList.remove('active');
+    function closeList(create, input) {
+        create.classList.remove('active');
         input.value = '';
     };
 
     function createList(inputValue) {
-        const listContainer = document.querySelector('.list-container');
-        const div = document.createElement('div');
-        div.classList.add('list');
-        div.innerHTML = list(inputValue);
-        listContainer.appendChild(div);
+        if (inputValue.value === "") return;
+        else {
+            const listContainer = document.querySelector('.list-container');
+            const div = document.createElement('div');
+            div.classList.add('list');
+            div.style.backgroundColor = input.style.backgroundColor;
+            div.innerHTML = list(inputValue.value);
+            listContainer.appendChild(div);
+        }
     };
+
+    function removeList(target) {
+        let list = target.parentElement.parentElement;
+        list.classList.add('remove');
+        list.addEventListener('transitionend', () => list.remove());
+    }
 
     function list(e) {
         return `<p>${e}</p>
@@ -48,58 +97,5 @@ function toDoListApp() {
                         <i class="fas fa-trash"></i>
                     </span>`
     };
-
-    function showEditAlert(target) {
-        const listValue = target.parentElement.previousElementSibling;
-        showInputAlert(listContainer, input);
-        SetText();
-        button.forEach(e => {
-            e.classList.contains('remove') ? e.remove() : listContainer.lastElementChild.appendChild(createBtnCancel);
-        });
-        input.value = listValue.textContent;
-    }
-
-    function SetText() {
-        const textInP = document.querySelector('.input p');
-        const button = document.querySelectorAll('.margin button');
-        const createBtnCancel = document.createElement('button');
-        createBtnCancel.classList.add('remove');
-        createBtnCancel.textContent = 'Cancel';
-        button[0].textContent = 'Edit List';
-        textInP.textContent = 'Edit List';
-    }
-
-    function defaultText() {
-        SetText;
-        createBtnCancel.remove();
-        button[0].textContent = 'Create List';
-        textInP.textContent = 'Create List';
-    }
-
-    function documentListener() {
-        document.addEventListener('click', function(e) {
-            let target = e.target;
-            if (target.classList.contains('fa-pen')) {
-                editable = true;
-                showEditAlert(target);
-                changeList(editable, target);
-            };
-        })
-    }
-    documentListener();
-
-    function changeList(editable, target) {
-        listContainer.addEventListener('click', function(e) {
-            if (e.target.id == 'create' && editable == true) {
-                const listValue = target.parentElement.previousElementSibling;
-                listValue.textContent = input.value;
-                closeList(listContainer, input);
-            } else if (e.target.id == 'create' && editable == false) {
-                createList(input.value);
-                closeList(listContainer, input);
-            }
-        })
-    };
-    changeList(editable);
 }
 toDoListApp();
