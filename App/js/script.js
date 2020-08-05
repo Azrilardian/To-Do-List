a7874f90c98118b381de2b638757b243c5c610cf
Azrilardian <irfan05ariell@gmail.com>
2020-08-05 23:47:21 +0800

    Update : Menambah fitur completed uncompleted

--- App/js/script.js
+++ App/js/script.js
@@ -7,7 +7,7 @@ function toDoListApp() {
     const closeIcon = document.querySelector('.create header p');
     const btnCreateList = document.querySelector('#create');
     const listColor = document.querySelectorAll('.color span');
-    const completedUncompleted = document.querySelector('#kategori1');
+    let colorName = 'white';
 
     // Listener
     btnShowInput.addEventListener('click', () => showInputAlert(create, input));
@@ -22,49 +22,16 @@ function toDoListApp() {
         closeList(create, input);
     });
     closeIcon.addEventListener('click', () => closeList(create, input));
-    listColor.forEach(color => {
-        color.addEventListener('click', function() {
-            const colorId = color.id;
-            switch (colorId) {
-                case "yellow":
-                    input.style.backgroundColor = '#ffffa9';
-                    input.focus();
-                    break;
-                case "green":
-                    input.style.backgroundColor = '#b4ffb4';
-                    input.focus();
-                    break;
-                case "blue":
-                    input.style.backgroundColor = '#a3ceff';
-                    input.focus();
-                    break;
-                case "black":
-                    input.style.backgroundColor = '#6d6d6d';
-                    input.focus();
-                    break;
-                case "grey":
-                    input.style.backgroundColor = '#d3d3d3';
-                    input.focus();
-                    break;
-                case "white":
-                    input.style.backgroundColor = '#ffffff';
-                    input.focus();
-                    break;
-            };
-            for (let i = 0; i < listColor.length; i++) listColor[i].classList.remove('active');
-            this.classList.add('active');
-        });
-    });
-
+    listColor.forEach(color => color.addEventListener('click', function() {
+        changeListColor(color);
+    }))
     document.addEventListener('click', e => {
         const target = e.target;
         if (target.classList.contains('list')) target.classList.toggle('completed');
         if (target.classList.contains('fa-trash')) removeList(target);
+        if (target.id == 'kategori1') fillterCompletedUncompleted(e);
+        if (target.id == 'kategori2') fillterByColor(e);
     })
-
-    completedUncompleted.addEventListener('click', e => fillterCompletedUncompleted(e));
-
-
 
     // Function
     const showInputAlert = (e, i) => {
@@ -82,7 +49,7 @@ function toDoListApp() {
         else {
             const listContainer = document.querySelector('.list-container');
             const div = document.createElement('div');
-            div.classList.add('list');
+            div.classList.add('list', colorName);
             div.style.backgroundColor = input.style.backgroundColor;
             div.innerHTML = list(inputValue.value);
             listContainer.appendChild(div);
@@ -105,6 +72,7 @@ function toDoListApp() {
 
     function fillterCompletedUncompleted(e) {
         const lists = document.querySelectorAll('.list');
+
         lists.forEach(list => {
             switch (e.target.value) {
                 case "Semua":
@@ -116,8 +84,80 @@ function toDoListApp() {
                 case "Belum Selesai":
                     !list.classList.contains('completed') ? list.style.display = 'flex' : list.style.display = 'none';
                     break;
-            };
+            }
         });
     };
+
+    function fillterByColor(e) {
+        const lists = document.querySelectorAll('.list');
+
+        function cekClass(list, className) {
+            list.classList.contains(className) ? list.style.display = 'flex' : list.style.display = 'none';
+        }
+
+        lists.forEach(list => {
+            switch (e.target.value) {
+                case "Berdasarkan Warna":
+                    list.style.display = 'flex';
+                    break;
+                case "Kuning":
+                    cekClass(list, 'yellow');
+                    break;
+                case "Hijau":
+                    cekClass(list, 'green');
+                    break;
+                case "Biru":
+                    cekClass(list, 'blue');
+                    break;
+                case "Hitam":
+                    cekClass(list, 'black');
+                    break;
+                case "Abu - abu":
+                    cekClass(list, 'grey');
+                    break;
+                case "Putih":
+                    cekClass(list, 'white');
+                    break;
+            };
+        });
+    }
+
+    function changeListColor(color) {
+        const colorId = color.id;
+        switch (colorId) {
+            case "yellow":
+                input.style.backgroundColor = '#ffffa9';
+                colorName = 'yellow';
+                input.focus();
+                break;
+            case "green":
+                input.style.backgroundColor = '#b4ffb4';
+                colorName = 'green';
+                input.focus();
+                break;
+            case "blue":
+                input.style.backgroundColor = '#a3ceff';
+                colorName = 'blue';
+                input.focus();
+                break;
+            case "black":
+                input.style.backgroundColor = '#6d6d6d';
+                colorName = 'black';
+                input.focus();
+                break;
+            case "grey":
+                input.style.backgroundColor = '#d3d3d3';
+                colorName = 'grey';
+                input.focus();
+                break;
+            case "white":
+                input.style.backgroundColor = '#ffffff';
+                colorName = 'white';
+                input.focus();
+                break;
+        };
+        for (let i = 0; i < listColor.length; i++) listColor[i].classList.remove('active');
+        color.classList.add('active');
+    }
 };
 toDoListApp();
