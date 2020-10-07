49e859aadfee1da18180624d0f7af770a1635e3b
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-10-07 11:49:58 +0800

    Update : Minify bundle.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,9 @@
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const terserPlugin = require("terser-webpack-plugin");
+const uglifyJs = require("uglifyjs-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "production",
@@ -19,5 +23,10 @@ module.exports = merge(common, {
 				],
 			},
 		],
+	},
+	plugins: [new OptimizeCSSAssetsPlugin(), new CleanWebpackPlugin()],
+	optimization: {
+		minimize: true,
+		minimizer: [new OptimizeCSSAssetsPlugin({ cssProcessorOptions: { map: { inline: false, annotation: true } } }), new terserPlugin(), new uglifyJs()],
 	},
 });
