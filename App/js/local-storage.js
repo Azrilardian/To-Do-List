cd45926a0e28b64504844dad56d10a53cd4ef5af
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-11-03 15:16:53 +0800

    Update : Memisahkan kode localStorage.js dan main.js

--- App/js/local-storage.js
+++ App/js/local-storage.js
@@ -1,19 +1,19 @@
 const STORAGE_TODO = "STORAGE TODO";
 let todos = {};
 function syncWithLocalStorage(activity, item, status = false, color, classColor) {
-    switch (activity) {
-        case "ADD":
-        case "UPDATE":
-            todos[item] = [status, color, classColor];
-            break;
-        case "DELETE":
-            delete todos[item];
-            break;
-        default:
-            break;
-    };
-    localStorage.setItem(STORAGE_TODO, JSON.stringify(todos));
-    return;
-};
+	switch (activity) {
+		case "ADD":
+		case "UPDATE":
+			todos[item] = [status, color, classColor];
+			break;
+		case "DELETE":
+			delete todos[item];
+			break;
+		default:
+			break;
+	}
+	localStorage.setItem(STORAGE_TODO, JSON.stringify(todos));
+	return;
+}
 
-export {STORAGE_TODO, syncWithLocalStorage};
+export { STORAGE_TODO, syncWithLocalStorage };
